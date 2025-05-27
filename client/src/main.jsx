ac1906c073a9de7718e99b2a780840ed9bfbf2d7
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-05-27 11:36:39 +0100

    frontend - store and initial api slice

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import './index.css'
 import App from './App.jsx'
+
+import store from './store.js'
+
 import HomePage from './pages/HomePage.jsx'
 
 const router = createBrowserRouter(
@@ -15,7 +19,9 @@ const router = createBrowserRouter(
 )
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>,
 )
