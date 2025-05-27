570eb1b34276a1dc79cf8bc47c86b06679d8810e
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-05-27 22:09:54 +0100

    frontend - login link in header

--- client/src/components/layout/HeaderContainer.jsx
+++ client/src/components/layout/HeaderContainer.jsx
@@ -1,10 +1,13 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { FaUser } from 'react-icons/fa'
 
 const smallScreenStyles = {
   padding: '0.75rem 0.75rem 0.5rem 0.75rem'
 }
 
 function HeaderContainer() {
+  const navigate = useNavigate()
   return (
     <header id="header-container" style={smallScreenStyles}>
       <nav id="main-nav">
@@ -12,7 +15,9 @@ function HeaderContainer() {
           <span id="logo"><em>OK</em>life</span>
         </Link>
         <ul className="main-nav-list" style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-          <li className="main-nav-item" style={{ padding: '0.25rem' }}></li>
+          <li className="main-nav-item" style={{ padding: '0.25rem', cursor: 'pointer' }} onClick={() => navigate('/login')}>
+            <FaUser style={{ fontSize: '22px' }} />
+          </li>
         </ul>
       </nav>
     </header>
