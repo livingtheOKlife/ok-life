d1d7ac79941e2002e89b282aff24beb2cd334234
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-05-30 21:16:13 +0100

    Revert "backend - initial email verification for register endpoint"
    This reverts commit 63f62e7c19efabe7ea19a1354fcf8ff9a5bd5a36.

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,5 +1,3 @@
-import emailTransporter from '../config/email.config.js'
-
 import User from '../models/user.model.js'
 
 import generateToken from '../utils/generateToken.js'
@@ -25,15 +23,6 @@ export const register = async (req, res) => {
     })
     if (user) {
       generateToken(res, user._id)
-      const token = Math.floor(100000 + Math.random() * 900000).toString()
-      await emailTransporter.sendMail({
-        from: process.env.EMAIL_ADDRESS,
-        to: email,
-        subject: 'Verify your email',
-        html: `
-          <span>Your verification token is ${token}</span>
-        `,
-      })
       res.status(201).json({
         success: true,
         message: 'User created successfully',
